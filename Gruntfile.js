b1fe5d71ba0594e9406973ade40aa61ad54e25be
Blake DeBaske <blakedebaske@gmail.com>
2016-03-17 01:12:26 -0400

    corrected architecture of files/folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,76 +3,61 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      // Sass task
-      sass: {
 
-          build: {
-              options: {
-                  outputStyle: 'compressed'
-              },
-              files: {
-                  'css/style.css': 'scss/style.scss'
-              }
-          },
-
-          dev: {
-              options: {
-                  outputStyle: 'expanded',
-                  sourceMap: true
-              },
-              files: {
-                  'css/style.css': 'scss/main.scss'
-              }
-          }
-      },
-    //Uglify Task  
-    uglify: {
+    //concat config
+    concat: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        separator: 'rn'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+
+      dist: {
+        src: ['js/modules/module1.js', 'js/modules/module2.js'],
+        dest: 'js/main.js'
       }
     },
 
-  // Post css
-  postcss: {
+    //Uglify config
+    uglify: {
       options: {
-          map: true, // inline sourcemaps
-
-          processors: [
-              require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-          ]
+        banner: '/*! &lt;%= pkg.name %&gt; &lt;%= grunt.template.today("dd-mm-yyyy") %&gt; */n'
       },
       dist: {
-          src: 'css/style.css'
+        files: {
+          'js/main.min.js': ['&lt;%= concat.dist.dest %&gt;']
+        }
       }
-  },
-
-  // Watch task
-  watch: {
-      css: {
-          options: {
-              livereload: true
-          },
-          files: ['scss/**/*.scss'],
-          tasks: ['sass:dev','postcss']
+    },
+    //JSHint config
+    jshint: {
+      files: ['gruntfile.js', 'js/*.js', 'js/modules/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
       }
-  },
+    },
+    //Concatenate and Compress SCSS: SCSS folder to CSS folder
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'scss',
+          cssDir: 'css',
+          environment: 'development',
+          outputStyle: 'compressed'
+        }
+      }
+    },
 
   });
 
   // Load the plugins
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks("grunt-modernizr");
-
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   // register task(s).
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['sass:dev','postcss']);
-  grunt.registerTask('build', ['sass:build','postcss']);
+  grunt.registerTask('default', ['concat', 'uglify', 'jshint','compass']);
 
 };
